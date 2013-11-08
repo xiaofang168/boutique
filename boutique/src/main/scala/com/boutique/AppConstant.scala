b1ad82ddfed25996318ea07ee5c70bbf33c4e62b
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-08 11:23:37 +0000

    增加了系统常量定义类

---
+++ boutique/src/main/scala/com/boutique/AppConstant.scala
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2013 杭州海康威视数字技术股份有限公司  All rights reserved.
+ * @Title: AppConstant.scala
+ * @Prject: boutique
+ * @Package: com.boutique
+ * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @date: 2013-11-8 下午5:08:31
+ * @version: V1.0
+ */
+package com.boutique
+
+/**
+ * @ClassName: AppConstant
+ * @Description: 应用常量定义
+ * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @date: 2013-11-8 下午5:08:31
+ * @version: V1.0
+ */
+object AppConstant {
+
+  val EHCACH_EABLE: Boolean = true
+  
+}
