07768f40c7c54b7feba2f3aa9f237496971328e0
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-08 11:26:53 +0000

    增加了dao测试类

---
+++ boutique/src/test/scala/com/boutique/dao/impl/CrudDAOImplTest.scala
@@ -0,0 +1,53 @@
+/**
+ * Copyright © 2013 杭州海康威视数字技术股份有限公司  All rights reserved.
+ * @Title: CrudDAOImplTest.java
+ * @Prject: boutique
+ * @Package: com.boutique.dao.impl
+ * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @date: 2013-11-8 下午2:56:21
+ * @version: V1.0
+ */
+package com.boutique.dao.impl;
+
+import org.junit.Test
+import com.boutique.Container
+import com.boutique.dao.CrudDAO
+import com.boutique.entities.User
+import javax.persistence.Entity
+import javax.persistence.Table
+import junit.framework.Assert
+
+/**
+ * @ClassName: CrudDAOImplTest
+ * @Description: 增删改查测试类
+ * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @date: 2013-11-8 下午2:56:21
+ * @version: V1.0
+ */
+class CrudDAOImplTest{
+
+	private var crudDao: CrudDAO  = Container.getInstance().getService(classOf[CrudDAO])
+	
+	@Test
+	def testSave() {
+		var user = new User
+		user.username = "fangjie"
+		crudDao.save(user)
+		Assert.assertNotNull(user.id)
+	}
+
+	
+	@Test
+	def testGet() {
+		var user = crudDao.get(classOf[User], 1l.asInstanceOf[java.io.Serializable])
+		Assert.assertEquals(1, user.id)
+	}
+	
+	@Test
+	def testFind(){
+	  testSave
+	  var list = crudDao.find("from User")
+	  Assert.assertEquals(1, list.size)
+	}
+
+}
