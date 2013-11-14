ab05f83a0c7eeaabcd397a848de6d143af22358e
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-14 11:57:00 +0000

    修改用户服务接口，拆分接口和实现类，增加了登录方法

---
+++ boutique/src/main/scala/com/boutique/services/user/internal/UserServiceImpl.scala
@@ -0,0 +1,30 @@
+
+package com.boutique.services.user.internal
+
+import com.boutique.services.user.UserService
+import com.boutique.dao.CrudDAO
+import org.apache.tapestry5.ioc.annotations.Inject
+import com.boutique.entities.User
+
+/**
+ * @ClassName: UserServiceImpl
+ * @Description: 用户服务实现
+ * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @date: 2013-11-14 下午6:26:23
+ * @version: V1.0
+ */
+class UserServiceImpl extends UserService {
+
+  @Inject
+  var crudDao: CrudDAO = _
+
+  def saved(user: User): User = {
+    return crudDao.save(user)
+  }
+
+  def login(username: String, password: String): Boolean = {
+    var list = crudDao.find("from User u where u.username = ? and u.password = ?", Array(username, password))
+    return !list.isEmpty
+  }
+  
+}
