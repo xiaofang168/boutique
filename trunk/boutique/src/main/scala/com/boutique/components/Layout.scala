9e0a8df7cb4538706ea2ea29a32e64b9f003f5e0
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-14 08:44:16 +0000

    新增components组件包

---
+++ trunk/boutique/src/main/scala/com/boutique/components/Layout.scala
@@ -0,0 +1,13 @@
+
+package com.boutique.components
+
+/**
+ * @ClassName: Layout
+ * @Description: TODO
+ * @author: 方杰  mailto:hbxffj@163.com
+ * @date: 2013-11-14 下午4:17:44
+ * @version: V1.0
+ */
+class Layout {
+
+}
