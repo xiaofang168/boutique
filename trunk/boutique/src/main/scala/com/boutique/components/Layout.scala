0cda566d8fd9c5715a8e99117822e0c9567df21c
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-14 14:17:17 +0000

    修改了注释

--- trunk/boutique/src/main/scala/com/boutique/components/Layout.scala
+++ trunk/boutique/src/main/scala/com/boutique/components/Layout.scala
@@ -3,8 +3,8 @@ package com.boutique.components
 
 /**
  * @ClassName: Layout
- * @Description: TODO
- * @author: 方杰  mailto:hbxffj@163.com
+ * @Description: 页面布局
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
  * @date: 2013-11-14 下午4:17:44
  * @version: V1.0
  */
