bdc6b63d746ef8b9ddaf4ee69ddac323b9d816bb
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-15 11:57:57 +0000

    判断用户是否登录

--- boutique/src/main/scala/com/boutique/pages/Index.scala
+++ boutique/src/main/scala/com/boutique/pages/Index.scala
@@ -1,7 +1,30 @@
 package com.boutique.pages
 
+import org.apache.tapestry5.annotations.Property
+import org.apache.tapestry5.ioc.annotations.Inject
+import org.apache.tapestry5.services.Request
+
+import com.boutique.AppConstant
+import com.boutique.entities.User
+import com.boutique.services.user.UserService
 
 class Index {
 
-  
+  @Property
+  private var user: User = _
+
+  @Inject
+  private var request: Request = _
+
+  @Inject
+  private var userService: UserService = _
+
+  def setupRender() {
+    var session = request.getSession(false)
+    if (session != null) {
+      if (session.getAttribute(AppConstant.USER_INFO) != null){
+    	  user = session.getAttribute(AppConstant.USER_INFO).asInstanceOf
+      }
+    }
+  }
 }
