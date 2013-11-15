468b581376ad93d5a851b36fc516961614bd6fe7
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-15 11:57:09 +0000

    增加了登录验证

--- trunk/boutique/src/main/scala/com/boutique/services/UserModule.scala
+++ trunk/boutique/src/main/scala/com/boutique/services/UserModule.scala
@@ -1,29 +1,31 @@
-/**
- * Copyright © 2013 杭州海康威视数字技术股份有限公司  All rights reserved.
- * @Title: UserModule.scala
- * @Prject: boutique
- * @Package: com.boutique.services
- * @author: 方杰  mailto:fangjie1@hikvision.com.cn
- * @date: 2013-11-4 下午7:37:49
- * @version: V1.0
- */
 package com.boutique.services
 
-import com.boutique.services.user.UserService
 import org.apache.tapestry5.ioc.ServiceBinder
+import com.boutique.services.user.UserService
+import com.boutique.services.user.internal.Authenticator
 import com.boutique.services.user.internal.UserServiceImpl
+import org.apache.tapestry5.ioc.annotations.Contribute
+import org.apache.tapestry5.ioc.MappedConfiguration
+import org.apache.tapestry5.validator.ValidatorMacro
 
 /**
  * @ClassName: UserModule
- * @Description: TODO
- * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @Description: 用户module
+ * @author:  <a href="mailto:hbxffj@163.com">方杰</a>
  * @date: 2013-11-4 下午7:37:49
  * @version: V1.0
  */
-class UserModule{}
+class UserModule {}
 object UserModule {
-  
-	def bind(binder: ServiceBinder ){
-        binder.bind(classOf[UserService], classOf[UserServiceImpl])
-    }
+
+  def bind(binder: ServiceBinder) {
+    binder.bind(classOf[UserService], classOf[UserServiceImpl])
+    binder.bind(classOf[Authenticator])
+  }
+
+  @Contribute(classOf[ValidatorMacro])
+  def combineValidators(configuration: MappedConfiguration[String, String]) {
+    configuration.add("username", "required, minlength=3, maxlength=15")
+    configuration.add("password", "required, minlength=6, maxlength=12")
+  }
 }
