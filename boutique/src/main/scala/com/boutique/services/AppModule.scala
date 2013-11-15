3f530ef95de9506e16b331d7034203b81ac759b2
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-15 11:56:38 +0000

    增加了语言，防止验证异常

--- boutique/src/main/scala/com/boutique/services/AppModule.scala
+++ boutique/src/main/scala/com/boutique/services/AppModule.scala
@@ -2,17 +2,15 @@ package com.boutique.services
 
 import org.apache.tapestry5.SymbolConstants
 import org.apache.tapestry5.hibernate.HibernateSymbols
-import org.apache.tapestry5.hibernate.HibernateTransactionAdvisor
+import org.apache.tapestry5.hibernate.HibernateTransactionDecorator
 import org.apache.tapestry5.ioc.Configuration
 import org.apache.tapestry5.ioc.MappedConfiguration
-import org.apache.tapestry5.ioc.MethodAdviceReceiver
+import org.apache.tapestry5.ioc.ServiceBinder
 import org.apache.tapestry5.ioc.annotations.Match
 import org.apache.tapestry5.ioc.annotations.SubModule
-import org.apache.tapestry5.hibernate.HibernateCoreModule
 import com.boutique.dao.CrudDAO
 import com.boutique.dao.impl.CrudDAOImpl
-import org.apache.tapestry5.ioc.ServiceBinder
-import org.apache.tapestry5.hibernate.HibernateTransactionDecorator
+import org.apache.tapestry5.hibernate.HibernateCoreModule
 
 @SubModule(Array(classOf[HibernateCoreModule], classOf[UserModule]))
 object AppModule {
@@ -24,13 +22,14 @@ object AppModule {
   //contribute factory defaults
   def contributeApplicationDefaults(configuration: MappedConfiguration[String, String]) {
     configuration.add(HibernateSymbols.EARLY_START_UP, "true");
+    configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+    configuration.add(SymbolConstants.APPLICATION_VERSION, "1.0-SNAPSHOT");
   }
 
   def contributeHibernateEntityPackageManager(configuration: Configuration[String]) {
     configuration.add("com.boutique.entities");
   }
 
-  
   @Match(Array[String]("*Service"))
   def decorateTransactions[T](serviceInterface: Class[T], delegate: T, serviceId: String, decorator: HibernateTransactionDecorator): T = {
     return decorator.build(serviceInterface, delegate, serviceId)
