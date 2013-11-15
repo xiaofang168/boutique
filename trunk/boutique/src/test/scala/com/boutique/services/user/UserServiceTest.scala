b3d7b3c441c08d163e3a31ac235d39b776006776
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-15 02:14:43 +0000

    增加了用户服务接口测试用例

---
+++ trunk/boutique/src/test/scala/com/boutique/services/user/UserServiceTest.scala
@@ -0,0 +1,48 @@
+package com.boutique.services.user
+
+import org.apache.tapestry5.hibernate.annotations.CommitAfter
+import org.junit.Assert
+import org.junit.Test
+
+import com.boutique.Container
+import com.boutique.dao.CrudDAO
+import com.boutique.entities.User
+
+import javax.persistence.Entity
+import javax.persistence.Table
+
+/**
+ * @ClassName: UserServiceTest
+ * @Description: TODO
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @date: 2013-11-15 上午9:39:06
+ * @version: V1.0
+ */
+class UserServiceTest {
+	
+	private var userService: UserService  = Container.getInstance().getService(classOf[UserService])
+	private var crudDao: CrudDAO  = Container.getInstance().getService(classOf[CrudDAO])
+	
+	
+	@Test
+	def testFind() {
+	   var list = crudDao.find("from User u")
+	   Assert.assertTrue(list.isEmpty)
+	}
+	
+	@Test
+	def testSave() {
+		var user = new User
+		user.username = "fangjie"
+		user.password = "1234"
+		userService.save(user)
+		Assert.assertNotNull(user.id)
+	}
+
+	@Test
+	def testLogin() {
+		var result = userService.login("fangjie", "1234")
+		Assert.assertTrue(result)
+	}
+
+}
