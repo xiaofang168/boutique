80f1de8a7c3df3e226df8358b1b45d99aeecab21
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-25 08:34:18 +0000

    修改了布局组件

--- trunk/boutique/src/main/scala/com/boutique/components/Layout.scala
+++ trunk/boutique/src/main/scala/com/boutique/components/Layout.scala
@@ -1,6 +1,15 @@
 
 package com.boutique.components
 
+import org.apache.tapestry5.annotations.Import
+import org.apache.tapestry5.annotations.Property
+import org.apache.tapestry5.ioc.annotations.Inject
+import org.apache.tapestry5.ComponentResources
+import com.boutique.services.user.internal.Authenticator
+import com.boutique.entities.User
+import org.apache.tapestry5.annotations.Log
+import com.boutique.pages.Index
+
 /**
  * @ClassName: Layout
  * @Description: 页面布局
@@ -8,6 +17,32 @@ package com.boutique.components
  * @date: 2013-11-14 下午4:17:44
  * @version: V1.0
  */
+@Import(library = Array(
+  "context:/static/js/bootstrap.min.js",
+  "context:/static/js/jquery.min.js",
+  "context:/static/js/CustomError.js"))
 class Layout {
 
+  @Property
+  private var pageName: String = _
+
+  @Inject
+  private var resources: ComponentResources = _
+
+  @Inject
+  private var authenticator: Authenticator = _
+
+  def getClassForPageName(): String = {
+    if (resources.getPageName().equalsIgnoreCase(pageName)) "current_page_item" else null
+  }
+
+  def getUser(): User = {
+    if (authenticator.isLoggedIn()) authenticator.getLoggedUser() else null
+  }
+
+  @Log
+  def onActionFromLogout(): Object = {
+    authenticator.logout()
+    return classOf[Index]
+  }
 }
