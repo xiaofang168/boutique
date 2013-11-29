a04e2cb37c7c307c87592253ed0dba4fd663ce71
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-29 18:21:05 +0000

    增加了jquery支持

--- boutique/src/main/scala/com/boutique/services/AppModule.scala
+++ boutique/src/main/scala/com/boutique/services/AppModule.scala
@@ -11,6 +11,7 @@ import org.apache.tapestry5.ioc.annotations.SubModule
 import com.boutique.dao.CrudDAO
 import com.boutique.dao.impl.CrudDAOImpl
 import org.apache.tapestry5.hibernate.HibernateCoreModule
+import org.got5.tapestry5.jquery.JQuerySymbolConstants
 
 @SubModule(Array(classOf[HibernateCoreModule], classOf[UserModule], classOf[WebModule]))
 object AppModule {
@@ -27,6 +28,12 @@ object AppModule {
     configuration.add(SymbolConstants.PRODUCTION_MODE, "false")
     configuration.add(SymbolConstants.HMAC_PASSPHRASE, "MD5")
     configuration.add(SymbolConstants.APPLICATION_VERSION, "1.0-SNAPSHOT")
+    // We have Tapestry5jQuery installed. Tell it we don't want it to suppress Prototype and Scriptaculous.
+    configuration.add(JQuerySymbolConstants.SUPPRESS_PROTOTYPE, "false");
+    // We don't use $j in our javascript - instead we use function scoping (see
+    // http://api.jquery.com/jQuery.noConflict/)
+    // but we need this next line to keep Tapestry happy (since Tapestry 5.3.4).
+    configuration.add(JQuerySymbolConstants.JQUERY_ALIAS, "$j");
   }
 
   def contributeHibernateEntityPackageManager(configuration: Configuration[String]) {
@@ -37,5 +44,5 @@ object AppModule {
   def decorateTransactions[T](serviceInterface: Class[T], delegate: T, serviceId: String, decorator: HibernateTransactionDecorator): T = {
     return decorator.build(serviceInterface, delegate, serviceId)
   }
-  
+
 }
