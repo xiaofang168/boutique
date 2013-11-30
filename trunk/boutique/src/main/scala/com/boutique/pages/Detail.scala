44118392747d1578914f4739ccf0fcc4fc704679
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2013-11-30 07:04:35 +0000

    添加了详情页面

---
+++ trunk/boutique/src/main/scala/com/boutique/pages/Detail.scala
@@ -0,0 +1,20 @@
+/**
+ * @FileName: Detail.scala
+ * @Prject:boutique
+ * @Package: com.boutique.pages
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @date: 2013-11-30
+ * @version: V1.0
+ */
+package com.boutique.pages
+
+/**
+ * @ClassName: Detail
+ * @Description: 详细信息页面
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @date: 下午2:34:52
+ * @version: V1.0
+ */
+class Detail {
+
+}
