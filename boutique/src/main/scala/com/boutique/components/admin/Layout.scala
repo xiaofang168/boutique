dadb3876208132a4c62a98756a9927f28f9b97a3
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2014-02-13 06:38:13 +0000

    管理员登录页面

---
+++ boutique/src/main/scala/com/boutique/components/admin/Layout.scala
@@ -0,0 +1,37 @@
+
+package com.boutique.components.admin
+
+import org.apache.tapestry5.annotations.Import
+import org.apache.tapestry5.annotations.Parameter
+import org.apache.tapestry5.annotations.Property
+import org.apache.tapestry5.ioc.annotations.Inject
+import com.boutique.entities.User
+import com.boutique.services.user.internal.Authenticator
+import javax.persistence.Entity
+import javax.persistence.Table
+import org.apache.tapestry5.BindingConstants
+
+/**
+ * @ClassName: Layout
+ * @Description: 页面布局
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @date: 2013-11-14 下午4:17:44
+ * @version: V1.0
+ */
+@Import(library = Array(
+  "context:/static/js/bootstrap.min.js",
+  "context:/static/js/CustomError.js"))
+class Layout {
+  
+  @Property
+  @Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
+  private var pageTitle: String = _
+
+  @Inject
+  private var authenticator: Authenticator = _
+  
+  def getUser(): User = {
+    if (authenticator.isLoggedIn()) authenticator.getLoggedUser() else null
+  }
+  
+}
