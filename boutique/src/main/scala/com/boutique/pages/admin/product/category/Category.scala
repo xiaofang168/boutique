0783c02ce94c55ac9ff8241d761de550e118ffce
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2014-02-24 10:23:04 +0000

    增加了产品列表及添加页面

---
+++ boutique/src/main/scala/com/boutique/pages/admin/product/category/Category.scala
@@ -0,0 +1,21 @@
+/**
+ * Copyright © 2014 杭州海康威视数字技术股份有限公司  All rights reserved.
+ * @Title: Category.scala
+ * @Prject: boutique
+ * @Package: com.boutique.pages.admin.product.category
+ * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @date: 2014-2-24 下午5:45:09
+ * @version: V1.0
+ */
+package com.boutique.pages.admin.product.category
+
+/**
+ * @ClassName: Category
+ * @Description: 产品类别描述
+ * @author: 方杰  mailto:fangjie1@hikvision.com.cn
+ * @date: 2014-2-24 下午5:45:09
+ * @version: V1.0
+ */
+class Category {
+
+}
