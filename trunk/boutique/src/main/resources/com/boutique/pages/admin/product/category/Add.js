192d1a4f6a041341fb5e5ad172c0f0aeb054721a
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2014-03-11 10:06:56 +0000

    消息国际化配置

---
+++ trunk/boutique/src/main/resources/com/boutique/pages/admin/product/category/Add.js
@@ -0,0 +1,6 @@
+var uploadImgTypeError = new HashMap();
+uploadImg.put("zh","上传图片格式不正确！");
+uploadImg.put("en","");
+
+var uploadImgMessage = new HashMap();
+uploadImgMessageHashMap.put("uploadImgTypeError",uploadImgTypeError);
