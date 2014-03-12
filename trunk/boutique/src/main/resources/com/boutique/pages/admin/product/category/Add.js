54e29612941bd74ad8d065b2b54e57f96741bf05
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2014-03-12 07:16:11 +0000

    移除页面引入js css文件

--- trunk/boutique/src/main/resources/com/boutique/pages/admin/product/category/Add.js
+++ trunk/boutique/src/main/resources/com/boutique/pages/admin/product/category/Add.js
@@ -1,6 +1,6 @@
 var uploadImgTypeError = new HashMap();
-uploadImg.put("zh","上传图片格式不正确！");
-uploadImg.put("en","");
+uploadImgTypeError.put("zh","上传图片格式不正确！");
+uploadImgTypeError.put("en","image type wrong!");
 
 var uploadImgMessage = new HashMap();
-uploadImgMessageHashMap.put("uploadImgTypeError",uploadImgTypeError);
+uploadImgMessage.put("uploadImgTypeError",uploadImgTypeError);
