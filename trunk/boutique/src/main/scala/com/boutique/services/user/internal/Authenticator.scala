524f4746891247e73c1274b7d2f9198d6505cbe9
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2014-03-18 10:48:32 +0000

    去掉了对session message的依赖

--- trunk/boutique/src/main/scala/com/boutique/services/user/internal/Authenticator.scala
+++ trunk/boutique/src/main/scala/com/boutique/services/user/internal/Authenticator.scala
@@ -1,16 +1,11 @@
 
 package com.boutique.services.user.internal
 
-import org.apache.tapestry5.services.Request
 import org.apache.tapestry5.ioc.annotations.Inject
-import org.apache.tapestry5.services.Session
-import com.boutique.AppConstant
-import com.boutique.entities.User
+
 import com.boutique.commons.AuthenticationException
 import com.boutique.dao.CrudDAO
-import org.apache.tapestry5.annotations.SessionState
-import org.apache.tapestry5.ioc.Messages
-import org.apache.tapestry5.annotations.SessionAttribute
+import com.boutique.entities.User
 
 /**
  * @ClassName: Authenticator
@@ -22,28 +17,12 @@ import org.apache.tapestry5.annotations.SessionAttribute
 class Authenticator {
 
   @Inject
-  private var messages: Messages = _
-
-  @Inject
   private var crudao: CrudDAO = _
 
-  @SessionAttribute("userInfo")
-  private var user: User = _
-
-  def isLoggedIn(): Boolean = {
-    return user != null
-  }
-
-  def logout() {
-    user = null
-  }
-
   @throws(classOf[AuthenticationException])
-  def login(username: String, password: String) = {
-    user = crudao.findUnique("from User u where u.username = ? or u.email=? and u.password = ?", Array(username, username, password))
-    if (user == null) {
-      throw new AuthenticationException(messages.get("error.usernotexists"))
-    }
+  def login(username: String, password: String):User = {
+    var user = crudao.findUnique[User]("from User u where u.username = ? or u.email=? and u.password = ?", Array(username, username, password))
+    return user
   }
 
 }
