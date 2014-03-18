4e526da79ba675f13477644db464d01396c475cf
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2014-03-18 10:52:06 +0000

    去掉了使用sessionstate

--- trunk/boutique/src/main/scala/com/boutique/pages/Index.scala
+++ trunk/boutique/src/main/scala/com/boutique/pages/Index.scala
@@ -1,18 +1,20 @@
 package com.boutique.pages
 
-import org.apache.tapestry5.annotations.SessionState
+import org.apache.tapestry5.annotations.Property
+import org.apache.tapestry5.annotations.SessionAttribute
 import org.apache.tapestry5.ioc.annotations.Inject
 import org.apache.tapestry5.services.Request
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import com.boutique.entities.User
 import com.boutique.services.user.UserService
 
-import javax.persistence.Entity
-import javax.persistence.Table
-
 class Index {
 
-  @SessionState
+  private var logger:Logger = LoggerFactory.getLogger(classOf[Index])
+   
+  @SessionAttribute("userInfo")
   private var user: User = _
 
   @Inject
@@ -20,5 +22,17 @@ class Index {
 
   @Inject
   private var userService: UserService = _
+  
+  @Property
+  var errorAction:String = _
+  
+  def onPassivate():String = { 
+	return errorAction
+  } 
+  
+  def onActivate(errorAction:String) {
+    this.errorAction = errorAction
+    logger.debug(this.errorAction)
+  }
 
 }
