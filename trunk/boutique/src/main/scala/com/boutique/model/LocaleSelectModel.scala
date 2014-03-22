4af5ff2e11ae7d90518432a5f3ea1274f87ca980
hbxffj168@gmail.com <hbxffj168@gmail.com@4b06ab0c-6945-2a88-b324-23e75e4140bf>
2014-03-22 14:07:15 +0000

    区分可变非可变list，使用可变ListBuffer

--- trunk/boutique/src/main/scala/com/boutique/model/LocaleSelectModel.scala
+++ trunk/boutique/src/main/scala/com/boutique/model/LocaleSelectModel.scala
@@ -9,11 +9,16 @@
  */
 package com.boutique.model
 
-import org.apache.tapestry5.util.AbstractSelectModel
+import java.util.Locale
+
+import scala.collection.JavaConverters.bufferAsJavaListConverter
+import scala.collection.mutable.ListBuffer
+
+import org.apache.tapestry5.OptionGroupModel
 import org.apache.tapestry5.OptionModel
 import org.apache.tapestry5.internal.OptionModelImpl
-import org.apache.tapestry5.OptionGroupModel
-import java.util.Locale
+import org.apache.tapestry5.util.AbstractSelectModel
+import scala.collection.JavaConverters._
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -24,19 +29,19 @@ import org.slf4j.LoggerFactory
  * @time: 下午4:10:16
  * @version: V1.0
  */
-class LocaleSelectModel(Locales: List[Locale]) extends  AbstractSelectModel{
-	
-	private var logger:Logger = LoggerFactory.getLogger(classOf[LocaleSelectModel])
-  
-    override def getOptionGroups(): java.util.List[OptionGroupModel] = {
-        return null
-    }
-    
-    override def getOptions():java.util.List[OptionModel] = {
-        var options = List[OptionModel]()
-        Locales.foreach(locale=>{
-          options ::=  new OptionModelImpl(locale.getDisplayName().replaceAll("zh_cn", "中文"),locale.getLanguage())
-        })
-        scala.collection.JavaConversions.asJavaList(options)
-    }
+class LocaleSelectModel(Locales: ListBuffer[Locale]) extends AbstractSelectModel {
+
+  private var logger: Logger = LoggerFactory.getLogger(classOf[LocaleSelectModel])
+
+  override def getOptionGroups(): java.util.List[OptionGroupModel] = {
+    return null
+  }
+
+  override def getOptions(): java.util.List[OptionModel] = {
+    var options = new scala.collection.mutable.ListBuffer[OptionModel]
+    Locales.foreach(locale => {
+      options += new OptionModelImpl(locale.getDisplayName().replaceAll("zh_cn", "中文"), locale.getLanguage())
+    })
+    return options.asJava
+  }
 }
